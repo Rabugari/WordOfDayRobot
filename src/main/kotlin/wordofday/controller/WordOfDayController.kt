717f13c27452e6e550aa5a28371de8578a166b7c
Rabugari <takaramassao@hotmail.com>
2019-03-11 23:18:48 -0300

    Update the crawler

--- src/main/kotlin/wordofday/controller/WordOfDayController.kt
+++ src/main/kotlin/wordofday/controller/WordOfDayController.kt
@@ -5,24 +5,37 @@ import org.jsoup.select.Elements
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import java.time.LocalDate
+
+const val WORD_OF_DAY_URL = "https://daily.wordreference.com"
 
 @RestController
 class WordOfDayController {
 
     @GetMapping("/crawler")
-    fun get(){
-        val wordOfDayPage = Jsoup.connect("https://daily.wordreference.com/").get()
+    fun get() {
+        val wordOfDayPage = Jsoup.connect(WORD_OF_DAY_URL).get()
         println(wordOfDayPage.title())
         val elements: Elements? = wordOfDayPage.select(".post-header h2 a")
         elements?.forEach {
             val wordDetailHref = it.attr("href")
 
-            println(it.text())
-            println("Link: $wordDetailHref")
-            val wordOfDayDetailPage = Jsoup.connect(wordDetailHref).get()
-            val divExamples = wordOfDayDetailPage.getElementsByClass("section list-w-title")
-            println("PAis? ${divExamples.parents().select("p")}")
-            println("Examples ${divExamples.select(".single ul li p").text()}")
+            if (isWordOfToday(wordDetailHref)) {
+                println("\n ${it.text()}")
+                val wordOfDayDetailPage = Jsoup.connect(wordDetailHref).get()
+
+                wordOfDayDetailPage.getElementsByClass("section list-w-title").select("li").forEach { example ->
+                    println("\n ${example.text()}")
+                }
+            }
+        }
+    }
+
+    private fun isWordOfToday(wordDetailHref: String): Boolean {
+        return LocalDate.now().let {
+            val month = if (it.monthValue < 10) "0${it.monthValue}" else "${it.monthValue}"
+            val dayOfMonth = if (it.dayOfMonth < 10) "0${it.dayOfMonth}" else "${it.dayOfMonth}"
+            wordDetailHref.startsWith("$WORD_OF_DAY_URL/${it.year}/$month/$dayOfMonth/")
         }
     }
 }
--- src/main/kotlin/wordofday/crawler/WordOfDayCrawler.kt
+++ src/main/kotlin/wordofday/crawler/WordOfDayCrawler.kt
@@ -1,7 +1,6 @@
 package wordofday.crawler
 
 import org.jsoup.Jsoup
-import org.jsoup.nodes.Element
 import org.jsoup.select.Elements
 
 class WordOfDayCrawler {
